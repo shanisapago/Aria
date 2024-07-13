14418dc460ae3e9e07067e9c07989957477429a5
NaamaYacov <118678355+NaamaYacov@users.noreply.github.com>
2024-07-13 19:31:56 +0300

    Update User.js

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,18 +1,10 @@
 import express from 'express';
-<<<<<<< HEAD
 import { addUser, getEvents, userCheck, checkUsername } from '../controllers/User.js';
 //import { addUser, userDetails } from '../controllers/User.js';
 console.log("inRoutes")
 const router = express.Router();
 router.post('/', addUser);
 router.get('/CheckUsername', checkUsername)
-=======
-import { addUser, getEvents, userCheck } from '../controllers/User.js';
-//import { addUser, userDetails } from '../controllers/User.js';
-console.log("inRoutes")
-const router = express.Router();
-router.post('/',addUser);
->>>>>>> 848f7af15d969dd6a2d6c7bb7d69ce781767d6bb
 router.get('/', getEvents);
 router.put('/Phones', userCheck);
 
